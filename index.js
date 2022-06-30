9d78e9ac1d8c9e5b6200757aefa1533baa81670d
Jaden Lund <lundjaden@gmail.com>
2022-06-30 14:45:24 -0400

    commit tree 77ffca702e2d5a82577e7eaacdc8704b78cd10e0

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(value1,value2){
+    return value1 + value2;
+}
+
+function subtract(value1,value2){
+    return value1 - value2;
+}
+
+function multiply(value1,value2){
+    return value1 * value2;
+}
+
+function divide(value1,value2){
+    return value1 / value2;
+}
+
+function increment(value1){
+    return ++value1;
+}
+
+function decrement(value1){
+    return --value1;
+}
+
+function makeInt(value1){
+    return parseInt(value1, 10);
+}
+
+function preserveDecimal(value1){
+   return parseFloat(value1)
+}
